1ab0a3b650e41f0f9e770af9ccb02dad3bfc8a90
Ashwyn Nair <ashwynnair@gmail.com>
2025-08-07 21:28:17 +0100

    Company hierarchy

--- init.sql
+++ init.sql
@@ -3,10 +3,37 @@ CREATE SCHEMA demo_app;
 
 SELECT pg_catalog.set_config('search_path', 'demo_app', false);
 
+-- Companies table
+CREATE TABLE demo_app.companies (
+  company_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  admin_id UUID NOT NULL REFERENCES demo_app.users(user_id) ON DELETE CASCADE,
+  name TEXT NOT NULL
+);
+
+-- Departments table
+CREATE TABLE demo_app.departments (
+  department_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  company_id UUID NOT NULL REFERENCES demo_app.companies(company_id) ON DELETE CASCADE,
+  head_of_department_id UUID REFERENCES demo_app.users(user_id) ON DELETE SET NULL
+);
+
+-- Teams table
+CREATE TABLE demo_app.teams (
+  team_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  department_id UUID NOT NULL REFERENCES demo_app.departments(department_id) ON DELETE CASCADE,
+  parent_team_id UUID REFERENCES demo_app.teams(team_id) ON DELETE SET NULL,
+  managed_by UUID REFERENCES demo_app.users(user_id) ON DELETE SET NULL,
+  company_id UUID NOT NULL REFERENCES demo_app.companies(company_id) ON DELETE CASCADE
+);
+
+-- Users table
 CREATE TABLE demo_app.users (
   user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT NOT NULL,
-  manager_id UUID REFERENCES demo_app.users
+  team_id UUID REFERENCES demo_app.teams(team_id) ON DELETE SET NULL,
+  company_id UUID NOT NULL REFERENCES demo_app.companies(company_id) ON DELETE CASCADE
 );
 
 CREATE TABLE demo_app.cards (
@@ -14,4 +41,14 @@ CREATE TABLE demo_app.cards (
   owner_id UUID NOT NULL REFERENCES demo_app.users
 );
 
-CREATE INDEX card_manager ON demo_app.cards (owner_id);
+CREATE INDEX idx_departments_company_id ON demo_app.departments(company_id);
+
+CREATE INDEX idx_teams_department_id ON demo_app.teams(department_id);
+CREATE INDEX idx_teams_parent_team_id ON demo_app.teams(parent_team_id);
+CREATE INDEX idx_teams_managed_by ON demo_app.teams(managed_by);
+CREATE INDEX idx_teams_company_id ON demo_app.teams(company_id);
+
+CREATE INDEX idx_users_team_id ON demo_app.users(team_id);
+CREATE INDEX idx_users_company_id ON demo_app.users(company_id);
+
+CREATE INDEX idx_cards_owner_id ON demo_app.cards(owner_id);
